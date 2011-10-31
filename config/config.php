821fb31550b7025f3e4eefc857ae33e59c86d57d
Lionel Besson <lionel.besson@gmail.com>
2011-10-31 09:06:34 +0000

    correct paths
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4094 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
 <?php
 // symfony directories
-$sf_symfony_lib_dir  = '/usr/share/php/symfony';
-$sf_symfony_data_dir = '/usr/share/php/data/symfony';
+$sf_symfony_lib_dir  = '/var/www/meta.camptocamp.org/lib/vendor/symfony/lib';
+$sf_symfony_data_dir = '/var/www/meta.camptocamp.org/lib/vendor/symfony/data';
