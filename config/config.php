19fbc74cbca19bdc7d62557d3f6fe9b0eee7b591
Marc Fournier <marc.fournier@camptocamp.org>
2011-11-02 07:20:34 +0000

    Fixed paths for prod in metaengine config files.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4098 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
 <?php
 // symfony directories
-$sf_symfony_lib_dir  = '/var/www/meta.camptocamp.org/lib/vendor/symfony/lib';
-$sf_symfony_data_dir = '/var/www/meta.camptocamp.org/lib/vendor/symfony/data';
+$sf_symfony_lib_dir  = '/srv/www/meta.camptocamp.org/lib/vendor/symfony/lib';
+$sf_symfony_data_dir = '/srv/www/meta.camptocamp.org/lib/vendor/symfony/data';
